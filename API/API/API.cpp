79881d1df219e8e48fdb97c04e81411963ddd94d
xGtx1304x <83380523+xGtx1304x@users.noreply.github.com>
2023-10-07 15:14:12 -0600

    Pruebas de tamaño de arreglo

--- API/API/API.cpp
+++ API/API/API.cpp
@@ -1,6 +1,18 @@
 #include <iostream>
 
+using namespace std;
+
+int size(string* arreglo) {
+    int res = 0;
+    while (arreglo[res] != "") {
+        res++;
+    }
+    return res;
+}
+
 int main()
 {
+    string* a = new string[6]{"ABC","DEF","123","987"};
+    cout << size(a);
     return 0;
 }
